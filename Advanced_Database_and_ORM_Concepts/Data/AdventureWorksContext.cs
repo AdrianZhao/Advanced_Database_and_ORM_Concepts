5a18a5c8a979980940bf169fa3208c544380c6eb
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-08-04 22:04:47 -0500

    update

---
+++ Advanced_Database_and_ORM_Concepts/Data/AdventureWorksContext.cs
@@ -0,0 +1,14 @@
+﻿using Advanced_Database_and_ORM_Concepts.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Advanced_Database_and_ORM_Concepts.Data
+{
+    public class AdventureWorksContext: DbContext
+    {
+        public AdventureWorksContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customer { get; set; } = null!;
+        public DbSet<Address> Address { get; set; } = null!;
+    }
+}
