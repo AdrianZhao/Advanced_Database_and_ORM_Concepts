fcc3d43451e58d029993adea770c986cde873e2d
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-08-04 21:59:34 -0500

    Add project files.

---
+++ Advanced_Database_and_ORM_Concepts/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Advanced_Database_and_ORM_Concepts.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
